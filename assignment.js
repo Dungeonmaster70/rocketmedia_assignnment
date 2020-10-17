3dd4a7caa3f714e1cee36cd6708f6fd851602d09
Dungeonmaster70 <60406530+Dungeonmaster70@users.noreply.github.com>
2020-10-17 13:43:18 +0530

    assignment completed

---
+++ assignment.js
@@ -0,0 +1,21 @@
+let num= Number(prompt()); //takes the number of socks
+
+let sock = prompt(); //takes color's  string seperated by space  
+
+//sorting the sock color in case needed  
+sock = sock.split(' ').sort(); 
+
+let cnt = pair = 0;
+
+let find_pair = () =>{
+  for(let i=0; i< num;i++){
+  cnt = Number(sock[i]);
+  if(cnt === Number(sock[i+1])){
+    pair += 1;
+    i++;
+  }
+}
+  return pair;
+}
+//pair of same colors are ->
+console.log(find_pair()); 
